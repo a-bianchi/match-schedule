bdb589e0e00dbaef73d75b36c231fea3ea747253
Alejo <alejobianchi@gmail.com>
2022-08-31 23:24:09 -0300

    feat: add protected routes

--- app/src/navigation/protected.route.tsx
+++ app/src/navigation/protected.route.tsx
@@ -1,16 +1,12 @@
 import { Navigate } from 'react-router-dom';
-import { SigninResponse } from '../interfaces';
 import { load } from '../utils/storage';
 import { KeyNames } from '../utils/storage';
 
 export const ProtectedRoute = ({ children }: any) => {
   const userToken = load(KeyNames.UserToken);
-  if (!userToken && userToken !== null) {
-    const { access_token } = userToken as SigninResponse;
-    if (access_token) {
-      // user is not authenticated
-      return <Navigate to="/" />;
-    }
+  if (!userToken || Object.keys(userToken).length === 0) {
+    // user is not authenticated
+    return <Navigate to="/" replace />;
   }
   return children;
 };
